4d730b7cb6581f9bb769d795dbf86c7da6dad732
Ratchanon <ratchanon.p@cjworx.com>
2021-11-12 16:35:25 +0700

    Get input from users

--- notes-app/app.js
+++ notes-app/app.js
@@ -9,3 +9,12 @@ console.log(getNotes())
 
 console.log(validator.isURL('https://www.mail.com'))
 console.log(chalk.red.bgBlue.bold('Success!'))
+console.log(process.argv)
+
+// ค่าที่อยู่หลังจาก run node eg. node app.js add (Section 4 ep.15)
+const command = process.argv[2]
+if(command === 'add') {
+  console.log('Adding note!')
+} else if(command === 'remove') {
+  console.log('Removing note!')
+}
